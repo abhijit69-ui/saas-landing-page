f573f52d6a761726a8c75969fdc30db959345689
Abhijit Nath <abhijitnath1a1@gmail.com>
2025-08-28 08:50:19 +0530

    Brands Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Layout from './components/Layout';
+import Brands from './components/sections/Brands';
 import Hero from './components/sections/Hero';
 
 const App = () => {
   return (
     <Layout title='EdgeAi'>
       <Hero />
+      <Brands />
     </Layout>
   );
 };
---
+++ src/components/shared/Title.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  children: React.ReactNode;
+}
+const Title = ({ children }: Props) => {
+  return (
+    <h1 className='text-heading-1 font-semibold text-2xl sm:text-3xl md:text-4xl'>
+      {children}
+    </h1>
+  );
+};
+
+export default Title;
